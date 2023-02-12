2dc2b02d5d8aac580cde14d64edecf6acae1753c
Dax <John-Xenos@yandex.ru>
2023-02-12 08:50:56 +0300

    Практическое задание 4

--- DaxWebToDo/UsersApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    nameuser = models.CharField(max_length=17, unique=True)
-    namefirst = models.CharField(max_length=77)
-    namelast = models.CharField(max_length=77)
-    email = models.EmailField(unique=True)
